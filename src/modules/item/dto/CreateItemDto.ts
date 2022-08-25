29ac6887c3f7804f14b1f7e6ed2302152ffef4ee
PGP27 <peg.gep27@gmail.com>
2022-08-25 12:27:27 -0300

    update

--- src/modules/item/dto/CreateItemDto.ts
+++ src/modules/item/dto/CreateItemDto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateItemDto {
   @IsNotEmpty({ message: "O título de um item é obrigatório" })
-  @MaxLength(100, { message: "Um item de uma lista deve ter no máximo 100 caracteres" })
+  @MaxLength(50, { message: "Um item de uma lista deve ter no máximo 50 caracteres" })
   text: string;
 
   @IsNotEmpty({ message: "O Id de uma lista é obrigatório para criar um item" })
