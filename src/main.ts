a9a981a409fda82eece38543c470dea10cd06546
pedro <peg.gep27@gmail.com>
2023-01-13 18:11:18 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: true,
-    methods: 'GET, PATCH, POST,DELETE',
-    credentials: true,
+    origin: ['https://take-notes-back.vercel.app'],
   });
   await app.listen(process.env.PORT || 3000);
 }
