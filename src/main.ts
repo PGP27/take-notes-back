6f8ebaa5d8c30b918ab79ca2e1d3ddc356af05ac
pedro <peg.gep27@gmail.com>
2023-02-06 11:34:24 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
+    allowedHeaders: ['content-type'],
+    origin: 'https://take-notes-front.vercel.app/',
     credentials: true,
-    allowedHeaders: ['Access-Control-Allow-Origin'],
   });
   await app.listen(process.env.PORT || 3000);
 }
